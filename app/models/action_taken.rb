fc698311ede72e2dc311600f4d6cad86bdc8d77b
mwatha <mwathabwanali@gmail.com>
2015-08-25 12:36:11 +0200

    Added new migrations and login page

--- app/models/action_taken.rb
+++ app/models/action_taken.rb
@@ -1,2 +1,4 @@
 class ActionTaken < ActiveRecord::Base
+  self.table_name = "action_taken"
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824140009) do
+ActiveRecord::Schema.define(version: 20150825085333) do
+
+  create_table "action_taken", primary_key: "action_id", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "action_type_id"
+    t.date     "action_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "action_type", primary_key: "action_type_id", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "encounter", primary_key: "encounter_id", force: true do |t|
+    t.integer  "encounter_type_id",  null: false
+    t.integer  "action_id"
+    t.datetime "encounter_datetime"
+    t.integer  "site_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "encounter_type", primary_key: "encounter_type_id", force: true do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "site", primary_key: "site_id", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sync_status", primary_key: "sync_status_id", force: true do |t|
+    t.integer  "site_id"
+    t.datetime "start_datetime", null: false
+    t.datetime "end_datetime"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "user_roles", id: false, force: true do |t|
+    t.integer  "user_id"
+    t.string   "role",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username",                      null: false
