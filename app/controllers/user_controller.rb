c327e494202895cd3638b3164c9a5aee4f9cc2cc
esther kajane soko <starjsoko@gmail.com>
2015-08-25 16:36:09 +0200

    working on user loggin 10% done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,14 @@
 class UserController < ApplicationController
   def login
+    if request.post?
+    	user = User.where(:username => params[:username]).first
+	unless user.blank?
+		if user.password_hash == params[:password_hash]
+		session[:user_id] = user.id
+		redirect_to "/"
+		end
+	end
+    end
+    session[:user_id] = nil
   end
 end
