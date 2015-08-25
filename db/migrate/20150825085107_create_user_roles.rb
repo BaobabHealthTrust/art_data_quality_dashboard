3e84dd624add341cf32760fc1e7bfebae12222ed
Jacob Mziya <jacobmziya@gmail.com>
2015-08-25 11:21:35 +0200

    adding table fields

--- db/migrate/20150825085107_create_user_roles.rb
+++ db/migrate/20150825085107_create_user_roles.rb
@@ -1,8 +1,12 @@
 class CreateUserRoles < ActiveRecord::Migration
   def change
     create_table :user_roles do |t|
+	t.integer :user_id, :nulll => false
+	t.string :role, :null => false
 
       t.timestamps
     end
+	remove_column :user_roles, :id
   end
+
 end
